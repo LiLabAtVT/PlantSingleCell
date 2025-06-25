f41cf95aad0817847ecd5330067a3cb4e3cec20b
RazanAj <162637339+RazanAj@users.noreply.github.com>
2025-06-25 15:02:40 -0400

    Update Step_4.2_Combind_FASTQ_Files.sh

--- Day1Section1/Step_4.2_Combind_FASTQ_Files.sh
+++ Day1Section1/Step_4.2_Combind_FASTQ_Files.sh
@@ -1,6 +1,5 @@
 
-#Run this command in your terminal. Make sure the FASTQ files is located in your cat 
-
-bamtofastq_S1_L002_R1_00{1..9}.fastq.gz bamtofastq_S1_L002_R1_010.fastq.gz > Pos24hpi1_S1_L001_R1_001.fastq.gz
+#Run this command in your terminal. Make sure the FASTQ files are located in your
+cat bamtofastq_S1_L002_R1_00{1..9}.fastq.gz bamtofastq_S1_L002_R1_010.fastq.gz > Pos24hpi1_S1_L001_R1_001.fastq.gz
 
 cat bamtofastq_S1_L002_R2_00{1..9}.fastq.gz bamtofastq_S1_L002_R2_010.fastq.gz > Pos24hpi1_S1_L001_R2_001.fastq.gz
